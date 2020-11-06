ea1a5565a23c3c56e4225c532bf953d555a620d1
hiimbalii <tcrbt1@gmail.com>
2020-11-06 19:18:53 +0100

    :white_check_mark: Writes tests

--- src/__tests__/Verseny.test.ts
+++ src/__tests__/Verseny.test.ts
@@ -12,4 +12,17 @@ describe('Verseny',()=>{
     it('should have valid indulos',()=>{
         expect(_verseny.Indulok.find(x=>x.LovesekSzama===0)).toBeFalsy()
     })
+    it('should only get indulo with 2 or more good shots in series',()=>{
+        _verseny.getAllWith2OrMoreShots().forEach(x=>{
+            expect(_verseny.Indulok[x-1].JoLovesek.length).toBeGreaterThanOrEqual(2);
+        })
+    })
+    it('should only contain indulo with less shots than the recorder',()=>{
+        _verseny.Indulok.forEach(x=>{
+            expect(x.LovesekSzama).toBeLessThanOrEqual(_verseny.Indulok[_verseny.getInduloWithMostShots()-1].LovesekSzama)
+        })
+    })
+    it('should get a list of indulok in appropiate formatting',()=>{
+        expect(_verseny.makeSorrend().length).toBe(_verseny.Indulok.length)
+    })
 })
